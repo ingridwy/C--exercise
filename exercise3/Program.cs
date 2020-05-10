f7429f33abc9e8cad6d1b548e60ebca8eb4cb8a5
Ingrid Cheng <bluebear589@gmail.com>
2020-05-10 15:10:39 +0100

    Prime factor found

---
+++ exercise3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long num = 600851475143;
+            for (long i=2; i<num; i++){
+                if (num % i ==0){
+                    bool isDivisor = true;
+                    for (long p=2 ;p<i; p++){
+                        if (i % p == 0) {
+                            isDivisor = false;
+                            break;
+                        }
+                    }
+                if (isDivisor) {
+                    long factor = i;
+                    Console.WriteLine(factor);
+                    
+                }
+                
+                }
+                
+            }
+            
+        }
+    }
+}
