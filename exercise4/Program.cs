3035cfcae1368882b877a81c33d0cba3082faf1b
Ingrid Cheng <bluebear589@gmail.com>
2020-05-13 17:10:35 +0100

    practice return function

---
+++ exercise4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(cube(6));
+        }
+        static int cube(int num){
+            int result = num*3;
+            return result;
+        }
+    }
+}
