e73092a658b5854c62d8d893c31040d68f63889b
Ingrid Cheng <bluebear589@gmail.com>
2020-05-14 15:26:57 +0100

    Practice if else statement

--- exercise4/Program.cs
+++ exercise4/Program.cs
@@ -7,6 +7,15 @@ namespace exercise4
         static void Main(string[] args)
         {
             Console.WriteLine(cube(6));
+            bool isDog = true;
+            if (isDog)
+            {
+                Console.WriteLine("This is a Dog!");
+            }
+            else
+            {
+                Console.WriteLine("This is not a Dog!");
+            }
         }
         static int cube(int num){
             int result = num*3;
