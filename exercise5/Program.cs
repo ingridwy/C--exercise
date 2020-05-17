2c1d633a5e21dbddfb62b47c3f89e5275500f095
Ingrid Cheng <bluebear589@gmail.com>
2020-05-17 16:57:21 +0100

    calculator

---
+++ exercise5/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            double num1
+        }
+    }
+}
