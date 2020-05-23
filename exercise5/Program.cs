4fed4c82789389a9dc9b54294cc2a234391fce9b
Ingrid Cheng <bluebear589@gmail.com>
2020-05-23 23:48:27 +0100

    simple program to find weekday from entering a number

--- exercise5/Program.cs
+++ exercise5/Program.cs
@@ -6,6 +6,8 @@ namespace exercise5
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(GetDay(0));
+            
             Console.Write("Enter a number:");
             double num1= Convert.ToDouble(Console.ReadLine());
 
@@ -32,6 +34,38 @@ namespace exercise5
             {
                 Console.WriteLine("Invalid Oerator");
             }
+
+        }
+        static string GetDay(int dayNum){
+            string dayName;
+            switch (dayNum)
+            {
+                case 0:
+                    dayName = "Sunday";
+                    break;
+                case 1:
+                    dayName = "Monday";
+                    break;
+                case 2:
+                    dayName = "Tuesday";
+                    break;
+                case 3:
+                    dayName = "Wednesday";
+                    break;
+                case 4:
+                    dayName = "Thursday";
+                    break;
+                case 5:
+                    dayName = "Friday";
+                    break;
+                case 6:
+                    dayName = "Saturday";
+                    break;
+                default:
+                    dayName = "Invalid entry";
+                    break;
+            }
+            return dayName;
         }
     }
 }
