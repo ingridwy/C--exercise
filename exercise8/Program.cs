2bc58c8a670e82c0769f900ba2cd4e05fc63d025
Ingrid Cheng <bluebear589@gmail.com>
2020-05-24 23:54:14 +0100

    for loop

---
+++ exercise8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace exercise8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int i =1;
+            while (i<=5){
+                Console.WriteLine(i);
+                i++;
+            }*/
+            for(int i = 1; i <=5; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
