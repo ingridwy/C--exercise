0af24e1383c775ae4a279c0390b0193b09a2382b
Ingrid Cheng <bluebear589@gmail.com>
2020-05-24 17:37:18 +0100

    while loop and do while loop

---
+++ exercise6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 1;
+            while (index <= 10)
+            {
+                Console.WriteLine(index);
+                index ++;
+            }
+             int index1 = 6;
+            do
+            {
+                Console.WriteLine(index1);
+                index1 ++;
+            } while (index1 <=5);
+        }
+    }
+}
