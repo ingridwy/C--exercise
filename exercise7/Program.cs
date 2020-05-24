39e6c02a667baf91d23a7b6a2cc0dc19bbba8b2c
Ingrid Cheng <bluebear589@gmail.com>
2020-05-24 19:26:44 +0100

    Simple guessing game

---
+++ exercise7/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace exercise7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secret = "Hippo";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 4;
+            bool outOfGuess = false;
+
+            while (guess !=secret && !outOfGuess)
+            {
+                if(guessCount < guessLimit){
+                    Console.Write("Enter your guess ");
+                    guess = Console.ReadLine();
+                    guessCount ++;
+                }
+                else
+                {
+                    outOfGuess = true;
+                }   
+            }
+            if(outOfGuess)
+                {
+                    Console.WriteLine("You Lose!");
+                }
+                else{
+                    Console.WriteLine("You win!");
+                }
+        }
+    }
+}
