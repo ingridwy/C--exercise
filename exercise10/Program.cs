38b966340f15d8212eaa560396ee5a5f746090ff
Ingrid Cheng <bluebear589@gmail.com>
2020-05-26 20:23:49 +0100

    object

---
+++ exercise10/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace exercise10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("James","chemistry", 3.2);
+            Student student2 = new Student("Mary","History",2.8);
+            
+            Console.WriteLine(student1.HasHonours());
+            Console.WriteLine(student2.HasHonours());
+        }
+    }
+}
---
+++ exercise10/student.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace exercise10
+{
+    class Student
+    {
+        public string name;
+        public string major;
+        public double gpa;
+        public Student(string aName, string aMajor,double aGpa)
+        {
+            name = aName;
+            major = aMajor;
+            gpa = aGpa;
+        }
+        public bool HasHonours()
+        {
+            if (gpa >= 3)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
