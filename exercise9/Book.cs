6e23ee927b279739bc7a984fbce3b084a52e4b79
Ingrid Cheng <bluebear589@gmail.com>
2020-05-26 16:44:55 +0100

    Constructors

--- exercise9/Book.cs
+++ exercise9/Book.cs
@@ -5,5 +5,12 @@ namespace exercise9{
          public string title;
          public string author;
          public int rating;
+         public Book(string aTitle, string anAuthor, int aRating)
+         {
+             Console.WriteLine("Creat a new book ");
+             title = aTitle;
+             author = anAuthor;
+             rating = aRating;
+         }
     }
 }
--- exercise9/Program.cs
+++ exercise9/Program.cs
@@ -6,17 +6,21 @@ namespace exercise9
     {
         static void Main(string[] args)
         {
-            Book book1 =new Book();
+            Book book1 = new Book("Sapien","Yuval Noah Harari", 5);
+            Book book2 = new Book("Homeo Deus","Yuval Noah Harari", 3);
+            Console.WriteLine(book1.title);
+
+           /* Book book1 =new Book("A:");
             book1.title ="Sapiens";
             book1.author = "Yuval Noah Harari";
             book1.rating = 5;
             Console.WriteLine(book1.title);
 
-            Book book2 =new Book();
+            Book book2 =new Book("B:");
             book2.title ="Homo Deus";
             book2.author = "Yuval Noah Harari";
             book2.rating = 3;
-            Console.WriteLine(book2.author);
+            Console.WriteLine(book2.author); */
         }
     }
 }
