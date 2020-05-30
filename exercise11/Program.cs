51465f5107c05628c0c71808695a89033abac183
Ingrid Cheng <bluebear589@gmail.com>
2020-05-30 23:57:34 +0100

    new song added

--- exercise11/Program.cs
+++ exercise11/Program.cs
@@ -8,6 +8,7 @@ namespace exercise11
         {
             Song attenetion = new Song ("Attenetion","Charlie Puth",20);
             Song liar = new Song("Liar", "Camila Cabello",200);
+            Song six = new Song("Six feet under","billie Ellish", 10);
         }
     }
 }
