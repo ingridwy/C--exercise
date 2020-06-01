64711faa21f767ad64932b89ecb73f48053b097a
Ingrid Cheng <bluebear589@gmail.com>
2020-06-01 21:52:11 +0100

    Static methods and claseese

---
+++ exercise12/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace exercise12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UsefulTools.SayHi("Sam");
+            Console.WriteLine(Math.Sqrt(144));
+        }
+    }
+}
---
+++ exercise12/UsefulTools.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace exercise12
+{
+    class UsefulTools
+    {
+        public static void SayHi(string name)
+        {
+            Console.WriteLine("Hi "+ name );
+        }
+    }
+}
